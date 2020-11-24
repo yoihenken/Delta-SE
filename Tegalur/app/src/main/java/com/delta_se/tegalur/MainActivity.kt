051c493087880713a1c2e1c1d0bca64974dd6f00
yoihenken <bagusbayu0310tkj1@gmail.com>
2020-11-25 05:59:08 +0700

    Create project

---
+++ Tegalur/app/src/main/java/com/delta_se/tegalur/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.delta_se.tegalur
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
